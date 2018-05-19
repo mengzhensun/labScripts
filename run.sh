62ce9e245ee196e4f0ba047c8cbb51db22000e0e
mengzhen <msmengzhen@gmail.com>
2018-05-18 20:06:30 -0400

    jobs failing problem

---
+++ run.sh
@@ -0,0 +1,2 @@
+SCHRODINGER=/home/friesner/ac3864/schrodinger_2018-1/
+$SCHRODINGER/fep_plus -HOST foct01 -SUBHOST gpu-desmond -JOBNAME mutation 5IES180508.mae -protein mutation.txt -solvent_asl 'chain.name C' -time 10000
