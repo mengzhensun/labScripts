d08bf7ba4b2987c1c16509d775269c9504dfab79
mengzhen <msmengzhen@gmail.com>
2018-06-07 17:43:15 -0400

    in mutation_4 dir, call this script to cp the comp and solvent graph into ../../graph

--- graphName.py
+++ graphName.py
@@ -1,6 +1,6 @@
 import commands
 import re
-f = open('mutation.edge', 'r')
+f = open('../mutation.edge', 'r')
 x = f.readlines()
 f.close
 pair = {}
@@ -13,16 +13,8 @@ mutationName = []
 for key, value in pair.iteritems():
     Name.append(value)
     mutationName.append(key[3:])
-# file1 = open('mutationNameGraph.txt', 'w')
-# file2 = open('graphName.txt', 'w')
-# for item in Name:
-#     file1.write("%s\n" % item)
-# for item in mutationName:
-#     file2.write("%s\n" % item) 
-# file1.close()
-# file2.close()
 for name in Name:
-    solName = "tar xzf *"+ name + "_slovent_9*"
+    solName = "tar xzf *"+ name + "_solvent_9*"
     compName = "tar xzf *" + name + "_complex_8*"
     output = commands.getoutput(solName)
     output = commands.getoutput(compName)
@@ -32,10 +24,10 @@ for name in Name:
     solName = "*" + name + "_solvent_"
     compName = "*" + name + "_complex_"
     graphName = mutationName[i]
-    solventCopy =  "cp " + solName + "fep1/" + solName + "*/freeenergy_time.png graph/" + graphName + ".png"
-    complexCopy =  "cp " + compName + "fep1/" + compName + "*/freeenergy_time.png graph/" + graphName + ".png"
+    solventCopy =  "cp " + solName + "fep1/" + solName + "*/freeenergy_time.png ../../graph/" + graphName +"sol"+ ".png"
+    complexCopy =  "cp " + compName + "fep1/" + compName + "*/freeenergy_time.png ../../graph/" + graphName +"comp"+ ".png"
     output = commands.getoutput(solventCopy)
     output = commands.getoutput(complexCopy)
     i += 1
-output = commands.getoutput("tar -cvzf graph.tgz ./graph")
+#output = commands.getoutput("tar -cvzf graph.tgz ./graph")
 
